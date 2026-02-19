563bfa1707ab367f77a99c43c7304bf5593e6208
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2026-02-19 14:43:05 +0330

    feat: Setup NavigationHost and integrate in MainActivity

--- app/src/main/java/net/ragham/candidateassessment/MainActivity.kt
+++ app/src/main/java/net/ragham/candidateassessment/MainActivity.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import net.ragham.candidateassessment.ui.invoice.InvoiceEditorScreen
+import net.ragham.candidateassessment.ui.invoice.InvoiceItemAddScreen
 import net.ragham.candidateassessment.ui.theme.CandidateAssessmentTheme
+import net.ragham.candidateassessment.viewmodel.InvoiceViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +21,65 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CandidateAssessmentTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // ایجاد و مدیریت NavController
+                val navController = rememberNavController()
+                // ایجاد ViewModel برای استفاده در کل نمودار ناوبری
+                val invoiceViewModel: InvoiceViewModel = viewModel()
+
+                NavHost(navController = navController, startDestination = "invoice_editor") {
+                    
+                    // صفحه اصلی: نمایش لیست آیتم‌ها
+                    composable("invoice_editor") {
+                        InvoiceEditorScreen(
+                            viewModel = invoiceViewModel,
+                            onAddItem = {
+                                navController.navigate("add_item")
+                            },
+                            onEditItem = { itemId ->
+                                navController.navigate("edit_item/$itemId")
+                            }
+                        )
+                    }
+
+                    // صفحه افزودن آیتم جدید
+                    composable("add_item") {
+                        InvoiceItemAddScreen(
+                            products = invoiceViewModel.products,
+                            initialItem = null,
+                            onSave = { newItem ->
+                                invoiceViewModel.addOrUpdateItem(newItem)
+                                navController.popBackStack()
+                            },
+                            onCancel = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+
+                    // صفحه ویرایش آیتم موجود
+                    composable(
+                        route = "edit_item/{itemId}",
+                        arguments = listOf(navArgument("itemId") { type = NavType.StringType })
+                    ) { backStackEntry ->
+                        val itemId = backStackEntry.arguments?.getString("itemId")
+                        val itemToEdit = itemId?.let { invoiceViewModel.getItem(it) }
+
+                        if (itemToEdit != null) {
+                            InvoiceItemAddScreen(
+                                products = invoiceViewModel.products,
+                                initialItem = itemToEdit,
+                                onSave = { updatedItem ->
+                                    invoiceViewModel.addOrUpdateItem(updatedItem)
+                                    navController.popBackStack()
+                                },
+                                onCancel = {
+                                    navController.popBackStack()
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CandidateAssessmentTheme {
-        Greeting("Android")
     }
 }
