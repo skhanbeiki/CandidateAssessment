273ec5aaf234e7f1b2dcfac1a6de1ce4d68ef8be
Moslem Khanbeiki <s.khanbeiki@gmail.com>
2026-02-19 12:01:52 +0330

    delete invoice data class

--- app/src/main/java/net/ragham/candidateassessment/viewmodel/InvoiceUiState.kt
+++
@@ -1,8 +0,0 @@
-package net.ragham.candidateassessment.viewmodel
-
-data class InvoiceUiState(
-    val customerName : String = "",
-    val customerPhone: String = "",
-    val items: List<InvoiceItem> = emptyList() ,
-
-)
